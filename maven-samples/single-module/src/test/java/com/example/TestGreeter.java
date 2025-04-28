6abfd40a72806284f5987a8c1db2bb6ebebb9de5
Sudhir Bera <sbera.connects@gmail.com>
2025-04-28 09:24:57 +0200

    Intentional fail test case

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -26,8 +26,12 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone), containsString(someone));
   }
-  
 
+  @Test
+  public void intentionallyFailed() {
+    assertTrue(false)
+  }
+  
   @Test
   public void greetShouldIncludeGreetingPhrase() {
     String someone = "World";
